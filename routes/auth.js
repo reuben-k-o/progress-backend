eb98939b8956bc7985d2d33c97f85ecd18bff8c1
reuben-k-o <rubenkhaemba@gmail.com>
2022-06-12 10:40:57 +0300

    created routes and controller

---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const authController = require("./controllers/auth");
+
+const route = express.Router();
+
+route.get("/signup", authController.getSignup);
+route.post("/signup", authController.postSignup);
+route.get("/login", authController.getLogin);
+route.post("/signup", authController.postLogin);
+
+module.exports = route;
---
+++ routes/personald_dashboard.js
@@ -0,0 +1,7 @@
+exports.getSignup = (req, res, next) => {};
+
+exports.postSignup = (req, res, next) => {};
+
+exports.getLogin = (req, res, next) => {};
+
+exports.postLogin = (req, res, next) => {};
